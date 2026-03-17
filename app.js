b8d263670bdc9f8ed28a618e3f2bf13643ee9d84
firdose <firdose0402@gmail.com>
2026-03-17 01:50:15 +0000

    Initial Node.js Docker app for DevOps project

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {"Content-Type":"text/html"});
+  res.end("<h1>Node.js App Running on Docker EC2</h1>");
+});
+
+server.listen(PORT,"0.0.0.0", () => {
+  console.log("Server running on port 3000");
+});
